a905744f932441390bb2e375ff8a03b410dca349
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2026-02-21 15:51:59 +0100

    EDIT: [Script] Keep list of engine units in EngineUnits.cmake

--- Core/CMake/EngineUnits.cmake
+++ Core/CMake/EngineUnits.cmake
@@ -5,3 +5,13 @@ include(CMake/EngineUnits/ECS.cmake)
 include(CMake/EngineUnits/Systems.cmake)
 
 include(CMake/EngineUnits/Components.cmake)
+
+list(APPEND ALL_ENGINE_UNITS
+        ${UTILITIES_PROJECT_NAME}
+        ${LOGGING_PROJECT_NAME}
+        ${ECS_PROJECT_NAME}
+        ${SYSTEMS_PROJECT_NAME}
+        ${COMPONENTS_PROJECT_NAME}
+)
+
+set(ALL_ENGINE_UNITS "${ALL_ENGINE_UNITS}" PARENT_SCOPE)
