bead15cb309c0c7122278e627f640b799c2861aa
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2026-02-23 11:24:52 +0100

    EDIT: [Script] Simple AssetFetcher implementation

--- Core/include/Asset/AssetFetcher/AssetFetcher.h
+++ Core/include/Asset/AssetFetcher/AssetFetcher.h
@@ -4,9 +4,26 @@
 
 #pragma once
 
+#include <filesystem>
+
 namespace rngo
 {
+    // TODO: Strongly typed class for RelativePath?
+
+    // Fetches paths relative to project path
+    // E.g "/home/user/Project/Textures/Grass.png" would be fetched using "Textures/Grass.png".
     class AssetFetcher
     {
+    public:
+        AssetFetcher(const std::filesystem::path& projectPath);
+
+        // Fetches using relative path, returns absolute path.
+        std::optional<std::filesystem::path> GetAssetPath(std::string_view relativePath);
+
+        // Gets relative path from an absolute path, nullopt if path is outside ProjectPath.
+        std::optional<std::filesystem::path> GetRelativePath(const std::filesystem::path& fullPath);
+
+    private:
+        std::filesystem::path m_projectPath;
     };
 }
--- Core/src/Asset/AssetFetcher/AssetFetcher.cpp
+++ Core/src/Asset/AssetFetcher/AssetFetcher.cpp
@@ -6,5 +6,32 @@
 
 namespace rngo
 {
+    AssetFetcher::AssetFetcher(const std::filesystem::path& projectPath)
+        : m_projectPath(projectPath)
+    {
+    }
 
+    std::optional<std::filesystem::path> AssetFetcher::GetAssetPath(const std::string_view relativePath)
+    {
+        const auto fullPath = m_projectPath / relativePath;
+        if (!std::filesystem::exists(fullPath))
+        {
+            return std::nullopt;
+        }
+
+        return fullPath;
+    }
+
+    std::optional<std::filesystem::path> AssetFetcher::GetRelativePath(const std::filesystem::path& fullPath)
+    {
+        const auto canonicalFullPath = std::filesystem::weakly_canonical(fullPath);
+        const auto canonicalProjectPath = std::filesystem::weakly_canonical(m_projectPath);
+
+        auto relative = std::filesystem::relative(canonicalFullPath, canonicalProjectPath);
+
+        if (relative.empty() || relative.string().starts_with(".."))
+            return std::nullopt;
+
+        return relative;
+    }
 }
